21e39a8fa6f8fabae49b53b550a1460beb5e042b
Bartosz Piechnik <bartoszpiechnik25@gmail.com>
2023-01-31 19:26:56 +0100

    Create threadpool class for asynchronus handling of the clients

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include "threadpool.h"
+#include <iostream>
+
+constexpr int N = 100000;
+static std::vector<int> mtrx(N);
+
+void initMatrix() {
+    for (auto i = 0; i < N; ++i)
+            mtrx.at(i) = 4;
+}
+std::mutex lockl;
+static unsigned long long sum = 0;
+static unsigned long long sum_pool = 0;
+
+void sumMatrix() {
+    for(auto i = 0; i < mtrx.size(); ++i)
+        sum += mtrx.at(i) + mtrx.at(i);
+}
+
+void sumMatrixPool() {
+    unsigned long long s = 0;
+    for(auto i = 0; i < mtrx.size(); ++i)
+        s += mtrx.at(i) + mtrx.at(i);
+    ulock l(lockl);
+    sum_pool += s;
+}
+
+using namespace std;
+
+int main(void) {
+    initMatrix();
+    ThreadPool pool;
+
+  // Measure time for ThreadPool execution
+  auto start = std::chrono::high_resolution_clock::now();
+  std::function<void()> job = std::bind(&sumMatrixPool);
+  for (int i = 0; i < N; ++i) {
+    pool.enqueue(job);
+  }
+//   pool.~ThreadPool();
+  pool.wait_for_jobs();
+  auto end = std::chrono::high_resolution_clock::now();
+  auto poolTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+  // Print results
+//   std::cout << "Sequential execution time: " << sequentialTime << " seconds" << "\nsequential ans: " << sum <<std::endl;
+  std::cout << "ThreadPool execution time: " << poolTime << " seconds" << "\npool ans: " << sum_pool <<std::endl;
+  return 0;
+}
---
+++ src/threadpool.cpp
@@ -0,0 +1,53 @@
+#include "threadpool.h"
+
+ThreadPool::ThreadPool(): done(false), busy(0) {
+    const unsigned num_threads = std::thread::hardware_concurrency();
+    for (auto i = 0; i < num_threads; ++i)
+        threads.emplace_back(std::thread(&ThreadPool::thread_loop, this));
+}
+
+ThreadPool::~ThreadPool() {
+    ulock lock(queue_lock);
+    done = true;
+    new_job.notify_all();
+    lock.unlock();
+    for (auto &task: threads)
+        task.join();
+}
+
+void ThreadPool::thread_loop() {
+    while (true) {
+        ulock lock(queue_lock);
+        new_job.wait(lock, [this] {
+            return done || !jobs.empty();
+        });
+
+        if (!jobs.empty()) {
+            ++busy;
+            auto function = std::move(jobs.front());
+            jobs.pop_front();
+
+            lock.unlock();
+
+            function();
+
+            lock.lock();
+            --busy;
+            finished_jobs.notify_one();
+
+        } else if (done) break;
+    }
+}
+
+void ThreadPool::wait_for_jobs() {
+    ulock lock(queue_lock);
+    finished_jobs.wait(lock, [this] {
+        return (busy == 0) && jobs.empty();
+    });
+}
+
+void ThreadPool::enqueue(std::function<void()>& job) {
+    ulock lock(queue_lock);
+    jobs.emplace_back(job);
+    new_job.notify_one();
+}
