1d28c12102c870380a3dad7ee9751466d0fc204f
Bartosz Piechnik <76113844+bartoszpiechnik25@users.noreply.github.com>
2023-02-01 20:08:26 +0100

    Threadpool (#1)
    * Create threadpool class for asynchronus handling of the clients
    
    * Cosmetic changes to the code

---
+++ include/threadpool.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <thread>
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+#include <vector>
+#include <functional>
+
+using ulock = std::unique_lock<std::mutex>;
+class ThreadPool {
+private:
+    std::deque <std::function<void()>> jobs;
+    std::mutex queue_lock;
+    std::condition_variable new_job;
+    std::condition_variable finished_jobs;
+    std::vector <std::thread> threads;
+    bool done;
+    unsigned int busy;
+    const unsigned num_threads;
+
+    void thread_loop();
+
+public:
+    explicit ThreadPool();
+
+    ~ThreadPool();
+
+    void wait_for_jobs();
+
+    void enqueue(std::function<void()> &job);
+
+    const unsigned getNumThreads() const noexcept;
+};
